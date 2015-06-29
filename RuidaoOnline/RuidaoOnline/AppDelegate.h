70fc546b2ae53b7cab4bb54fee1cbc075d1a0769
Heguofeng <xxx@qq.com>
2015-06-29 10:04:56 +0800

    睿道在线APP项目 Create

---
+++ RuidaoOnline/RuidaoOnline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuidaoOnline
+//
+//  Created by hegf on 15/6/29.
+//  Copyright (c) 2015年 hegf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
